b9dc831d5af278d930f1580d45a515a26c3055cc
scar17off <81940406+scar17off@users.noreply.github.com>
2021-12-01 21:47:38 +0200

    Delete js directory

--- js/SoundOnDemand.js
+++
@@ -1,7 +0,0 @@
-let xhttpt = new XMLHttpRequest();
-xhttpt.open("GET", "https://shotwars.io/js/SoundOnDemand.js");
-xhttpt.responseType = "text";
-xhttpt.addEventListener("load", function() {
-	eval(xhttpt.response);
-});
-xhttpt.send();
--- js/c197.js
+++
@@ -1,7 +0,0 @@
-let xhttpt = new XMLHttpRequest();
-xhttpt.open("GET", "https://shotwars.io/js/c197.js");
-xhttpt.responseType = "text";
-xhttpt.addEventListener("load", function() {
-	eval(xhttpt.response);
-});
-xhttpt.send();
