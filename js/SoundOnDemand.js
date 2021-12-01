4000a749b2105a68b007a825025bba350270d860
scar17off <81940406+scar17off@users.noreply.github.com>
2021-12-01 21:48:54 +0200

    Add files via upload

---
+++ js/SoundOnDemand.js
@@ -0,0 +1,814 @@
+/*     
+
+  SoundOnDemand r1
+
+  (c) 2012-2015 http://rezoner.net
+
+  This library may be freely distributed under the MIT license.
+
+*/
+/* options */
+
+/* output: output node, default */
+/* audioContext: audioContext */
+
+if (!window.AudioContext) {
+
+  window.AudioContext = window.webkitAudioContext;
+
+  if (!window.AudioContext.prototype.createStereoPanner) {
+
+    window.AudioContext.prototype.createStereoPanner = function() {
+
+      var node = this.createGain();
+
+      node.pan = {
+        value: 0
+      };
+
+      return node;
+
+    }
+
+  }
+
+}
+
+window.SoundOnDemand = function(options) {
+
+  options = options || {};
+
+  var canPlayMp3 = (new Audio).canPlayType("audio/mp3");
+  var canPlayOgg = (new Audio).canPlayType('audio/ogg; codecs="vorbis"');
+
+  if (this.preferedAudioFormat === "mp3") {
+
+    if (canPlayMp3) this.audioFormat = "mp3";
+    else this.audioFormat = "ogg";
+
+  } else {
+
+    if (canPlayOgg) this.audioFormat = "ogg";
+    else this.audioFormat = "mp3";
+
+  }
+
+  this.audioContext = options.audioContext || new AudioContext;
+
+  this.compressor = this.audioContext.createDynamicsCompressor();
+  this.compressor.connect(this.audioContext.destination);
+
+  this.gainNode = this.audioContext.createGain()
+  this.gainNode.connect(this.compressor);
+
+  this.input = this.gainNode;
+
+  this.gainNode.gain.value = 1.0;
+
+  this.buffers = {};
+
+  this.channels = {};
+  this.aliases = {};
+
+  var lastTick = Date.now();
+  var engine = this;
+
+  setInterval(function() {
+
+    var delta = (Date.now() - lastTick) / 1000;
+
+    lastTick = Date.now();
+
+    engine.step(delta);
+
+  }, 1000 / 30);
+
+};
+
+SoundOnDemand.moveTo = function(value, target, step) {
+
+  if (value < target) {
+    value += step;
+    if (value > target) value = target;
+  }
+
+  if (value > target) {
+    value -= step;
+    if (value < target) value = target;
+  }
+
+  return value;
+
+};
+
+SoundOnDemand.prototype = {
+
+  constructor: SoundOnDemand,
+
+  path: "sounds/",
+
+  channel: function(name) {
+
+    if (!this.channels[name]) this.channels[name] = new SoundOnDemand.Channel(this);
+
+    return this.channels[name];
+
+  },
+
+  getAssetEntry: function(path, defaultExtension) {
+
+    /* translate folder according to user provided paths 
+       or leave as is */
+
+    var fileinfo = path.match(/(.*)\..*/);
+    var key = fileinfo ? fileinfo[1] : path;
+
+    var temp = path.split(".");
+    var basename = path;
+
+    if (temp.length > 1) {
+      var ext = temp.pop();
+      path = temp.join(".");
+    } else {
+      var ext = defaultExtension;
+      basename += "." + defaultExtension;
+    }
+
+    var url;
+
+    if (false) {
+
+    //  url = app.rewriteURL(this.path + basename);
+
+    } else {
+
+      url = this.path + basename;
+
+    }
+
+    return {
+      key: key,
+      url: url,
+      path: this.path + path,
+      ext: ext
+    };
+
+  },
+
+  loaders: {},
+
+  load: function(key) {
+
+    var engine = this;
+    var entry = engine.getAssetEntry(key, engine.audioFormat);
+
+    if (!this.loaders[key]) {
+
+      this.loaders[key] = new Promise(function(resolve, reject) {
+
+        if (engine.buffers[entry.key]) return resolve(engine.buffers[entry.key]);
+
+        var request = new XMLHttpRequest();
+
+        request.open("GET", entry.url, true);
+        request.responseType = "arraybuffer";
+
+        request.onload = function() {
+
+          engine.audioContext.decodeAudioData(this.response, function(decodedBuffer) {
+
+            engine.buffers[entry.key] = decodedBuffer;
+            resolve(decodedBuffer);
+
+          });
+
+        }
+
+        request.send();
+
+      });
+
+    }
+
+    return this.loaders[key];
+
+  },
+
+  step: function(delta) {
+
+    for (var key in this.channels) {
+
+      this.channels[key].step(delta);
+
+    }
+
+  },
+
+  duplicate: function(source, as, volume, rate) {
+
+    var engine = this;
+
+    this.load(source).then(function() {
+
+      engine.buffers[source];
+
+      engine.buffers[as] = engine.buffers[source];
+
+    });
+
+  },
+
+  alias: function(name, source, rate, volume) {
+
+    this.aliases[name] = {
+      source: source,
+      rate: rate,
+      volume: volume
+    };
+
+  }
+
+};SoundOnDemand.Events = function() {
+
+  this.listeners = {};
+
+};
+
+SoundOnDemand.Events.prototype = {
+
+  on: function(event, callback) {
+
+    if (typeof event === "object") {
+      var result = {};
+      for (var key in event) {
+        result[key] = this.on(key, event[key])
+      }
+      return result;
+    }
+
+    if (!this.listeners[event]) this.listeners[event] = [];
+
+    this.listeners[event].push(callback);
+
+    return callback;
+  },
+
+  once: function(event, callback) {
+
+    callback.once = true;
+
+    if (!this.listeners[event]) this.listeners[event] = [];
+
+    this.listeners[event].push(callback);
+
+    return callback;
+
+  },
+
+  off: function(event, callback) {
+
+    for (var i = 0, len = this.listeners[event].length; i < len; i++) {
+      if (this.listeners[event][i]._remove) {
+        this.listeners[event].splice(i--, 1);
+        len--;
+      }
+    }
+
+  },
+
+  trigger: function(event, data) {
+
+    /* if you prefer events pipe */
+
+    if (this.listeners["event"]) {
+      for (var i = 0, len = this.listeners["event"].length; i < len; i++) {
+        this.listeners["event"][i](event, data);
+      }
+    }
+
+    /* or subscribed to single event */
+
+    if (this.listeners[event]) {
+      for (var i = 0, len = this.listeners[event].length; i < len; i++) {
+        var listener = this.listeners[event][i];
+        listener.call(this, data);
+
+        if (listener.once) {
+          this.listeners[event].splice(i--, 1);
+          len--;
+        }
+      }
+    }
+
+  }
+
+};SoundOnDemand.Channel = function(engine) {
+
+  this.engine = engine;
+  this.audioContext = engine.audioContext;
+
+  /* connection order goes from bottom to top */
+
+  /* gain node */
+
+  this.gainNode = this.audioContext.createGain();
+
+  /* convolver */
+
+  this.convolverWetNode = this.audioContext.createGain();
+  this.convolverDryNode = this.audioContext.createGain();
+  this.convolverNode = this.audioContext.createConvolver();
+  this.convolverEnabled = false;
+
+  this.route();
+
+  this.queue = [];
+  this.loops = [];
+
+};
+
+SoundOnDemand.Channel.prototype = {
+
+  constructor: SoundOnDemand.Channel,
+
+  get: function(key) {
+
+    return new SoundOnDemand.Sound(key, this);
+
+  },
+
+  play: function(key) {
+
+    if (this.mutedWith) key = this.mutedWith;
+
+    var sound = this.get(key);
+
+    this.add(sound);
+
+    return sound;
+
+  },
+
+  remove: function(sound) {
+
+    sound._remove = true;
+
+  },
+
+  add: function(sound) {
+
+    sound._remove = false;
+
+    if (this.queue.indexOf(sound) > -1) return;
+
+    this.queue.push(sound);
+
+  },
+
+  step: function(delta) {
+
+    /* process queue */
+
+    for (var i = 0; i < this.queue.length; i++) {
+
+      var sound = this.queue[i];
+
+      if (sound._remove) {
+      
+        this.queue.splice(i--, 1);
+      
+        continue;
+
+      }
+
+      sound.step(delta);
+
+    }
+
+    /* process sounds being played */
+
+  },
+
+  muteWith: function(key) {
+
+    this.mutedWith = key;
+
+  },
+
+  volume: function(value) {
+
+    this.gainNode.gain.value = value;
+
+    return this;
+
+  },
+
+  swapConvolver: function(key) {
+
+    var engine = this.engine;
+    var channel = this;
+
+    return new Promise(function(resolve, fail) {
+
+      if (channel.currentConvolverImpulse === key) {
+
+        resolve();
+
+      } else {
+
+        engine.load(key).then(function(buffer) {
+          channel.currentConvolverImpulse = key;
+          channel.convolverNode.buffer = buffer;
+          resolve();
+        });
+
+      }
+
+    });
+
+  },
+
+  updateConvovlerState: function(enabled) {
+
+    this.convolverEnabled = enabled;
+    this.route();
+
+  },
+
+  subroute: function(nodes) {
+
+    for (var i = 0; i < nodes.length; i++) {
+
+      if (i < nodes.length - 1) {
+
+        var node = nodes[i];
+        node.disconnect();
+        node.connect(nodes[i + 1]);
+
+      }
+
+    }
+
+    this.input = nodes[0];
+
+  },
+
+  route: function() {
+
+    this.gainNode.disconnect();
+
+    if (this.convolverEnabled) {
+
+      this.gainNode.connect(this.convolverDryNode);
+
+      this.gainNode.connect(this.convolverNode);
+      this.convolverNode.connect(this.convolverWetNode);
+
+      this.convolverWetNode.connect(this.engine.input);
+      this.convolverDryNode.connect(this.engine.input);
+
+    } else {
+
+      this.gainNode.connect(this.engine.input);
+
+    }
+
+    this.input = this.gainNode;
+
+  },
+
+  convolver: function(value, key) {
+
+    var enabled = value > 0;
+    var channel = this;
+
+    this.swapConvolver(key).then(function() {
+
+      if (enabled !== channel.convolverEnabled) channel.updateConvovlerState(enabled);
+
+    });
+
+    this.convolverWetNode.gain.value = value;
+    this.convolverDryNode.gain.value = 1 - value;
+
+    return this;
+
+  }
+
+};SoundOnDemand.Sound = function(key, channel) {
+
+  this.key = key;
+  this.bufferKey = key;
+  this.delayTimeout = 0;
+  this.paused = false;
+
+  if (channel.engine.aliases[key]) {
+
+    this.alias = channel.engine.aliases[key];
+
+    this.bufferKey = this.alias.source;
+
+  }
+
+  if (!channel.engine.buffers[this.bufferKey]) channel.engine.load(this.bufferKey);
+
+  this.channel = channel;
+  this.audioContext = this.channel.engine.audioContext;
+
+  this.current = {
+    volume: 1.0,
+    rate: 1.0
+  };
+
+  this.fadeMod = 1.0;
+
+  this.createNodes();
+
+};
+
+SoundOnDemand.Sound.prototype = {
+
+  constructor: SoundOnDemand.Sound,
+
+  alias: {
+    volume: 1.0,
+    rate: 1.0
+  },
+
+  createNodes: function() {
+
+    var bufferSource = this.audioContext.createBufferSource();
+    var gainNode = this.audioContext.createGain();
+    var panNode = this.audioContext.createStereoPanner();
+
+    bufferSource.connect(panNode);
+    panNode.connect(gainNode);
+    gainNode.connect(this.channel.input);
+
+    this.bufferSource = bufferSource;
+    this.gainNode = gainNode;
+    this.panNode = panNode;
+
+  },
+
+  volume: function(volume) {
+
+    volume *= this.alias.volume;
+
+    this.current.volume = volume;
+
+    this.updateVolume();
+
+    return this;
+
+  },
+
+  volumef: function(volume) {
+
+    return this.volume(this.current.volume * volume);
+
+  },
+
+  delay: function(time) {
+
+    this.delayTimeout = time;
+
+    return this;
+
+  },
+
+  updateVolume: function() {
+
+    this.gainNode.gain.value = this.current.volume * this.fadeMod;
+
+  },
+
+  pan: function(pan) {
+
+    if (pan < -1.0) pan = -1.0;
+    if (pan > 1.0) pan = 1.0;
+
+    this.current.pan = pan;
+
+    this.updatePanning();
+
+    return this;
+
+  },
+
+  updatePanning: function() {
+
+    this.panNode.pan.value = this.current.pan;
+
+  },
+
+  loop: function(start = 0, end = 0) {
+
+    this.bufferSource.loop = true;
+    this.current.loop = true;
+
+    this.bufferSource.loopStart = start;
+    if (end) this.bufferSource.loopEnd = end;
+
+    return this;
+
+  },
+
+  rrate: function(range) {
+
+    return this.rate(this.current.rate + (-1 + Math.random() * 2) * range);
+
+  },
+
+  nrate: function(range) {
+
+    return this.rate(this.current.rate - Math.random() * range);
+
+  },
+
+  prate: function(range) {
+
+    return this.rate(this.current.rate + Math.random() * range);
+
+  },
+
+  rate: function(rate) {
+
+    rate *= this.alias.rate;
+
+    this.bufferSource.playbackRate.value = rate;
+
+    this.current.rate = rate;
+
+    return this;
+
+  },
+
+  rateTo: function(target, duration) {
+
+    if (!this.playing && this.ready) this.resume();
+
+    duration = duration || 1.0;
+
+    this.rateTime = 0;
+    this.rateTarget = target;
+    this.rateDuration = duration;
+
+    this.rateSpeed = Math.abs(target - this.current.rate) / duration;
+
+    return this;
+
+  },
+
+  onended: function() {
+
+    if (!this.current.loop) this.stop();
+
+  },
+
+  step: function(delta) {
+
+    let updateVolume = false;
+
+    if (this.delayTimeout > 0) {
+
+      this.delayTimeout -= delta;
+
+      return;
+
+    }
+
+    if (this.fadeTarget !== this.fadeMod) {
+
+      updateVolume = true;
+
+      this.fadeMod = SoundOnDemand.moveTo(this.fadeMod, this.fadeTarget, delta * this.fadeSpeed);
+
+    } else if (this.fadeTarget === 0) {
+
+      this.pause();
+
+      return;
+
+    }
+
+    if (!this.ready) {
+
+      if (!this.channel.engine.buffers[this.bufferKey]) return;
+
+      this.ready = true;
+      this.playing = true;
+
+      this.buffer = this.channel.engine.buffers[this.bufferKey];
+
+      this.bufferSource.buffer = this.buffer;
+
+      this.bufferSource.start(0);
+      this.bufferSource.onended = this.onended.bind(this);
+
+      this.currentTime = 0;
+
+      this.currentTime += this.bufferSource.playbackRate.value * delta;
+
+    }
+
+    if (updateVolume) this.updateVolume();
+
+    if (this.rateTarget !== this.current.rate) {
+
+      this.rate(SoundOnDemand.moveTo(this.current.rate, this.rateTarget, delta * this.rateSpeed));
+
+    }
+
+  },
+
+  pause: function() {
+
+    this.channel.remove(this);
+
+    if (this.playing) this.bufferSource.stop(0);
+
+    this.playing = false;
+
+  },
+
+  stop: function() {
+
+    this.channel.remove(this);
+
+    if (!this.playing) return;
+
+    this.bufferSource.stop(0);
+
+    this.playing = false;
+
+  },
+
+  play: function() {
+
+    if (this.playing) return;
+
+    this.createNodes();
+
+    this.channel.add(this);
+
+    this.ready = false;
+    this.playing = false;
+
+  },
+
+  resume: function() {
+
+    this.createNodes();
+
+    this.bufferSource.buffer = this.buffer;
+
+    this.currentTime = this.currentTime % this.buffer.duration;
+    this.bufferSource.start(0, this.currentTime);
+
+    this.rate(this.current.rate);
+    this.volume(this.current.volume);
+    this.loop(this.current.loop);
+
+    this.channel.add(this);
+
+    this.playing = true;
+
+  },
+
+  fadeTo: function(target, duration) {
+
+    if (!this.playing && this.ready) this.resume();
+
+    duration = duration || 1.0;
+
+    this.fadeTime = 0;
+    this.fadeTarget = target;
+    this.fadeDuration = duration;
+
+    this.fadeSpeed = Math.abs(target - this.fadeMod) / duration;
+
+    return this;
+
+  },
+
+  fadeIn: function(duration) {
+
+    if (!this.playing && this.ready) this.resume();
+
+    this.fadeMod = 0;
+    this.fadeTo(1.0, duration);
+
+    return this;
+
+  },
+
+  fadeOut: function(duration) {
+
+    this.fadeTo(0, duration || 1.0);
+
+    return this;
+
+  }
+
+};
